624b14632356b4643e114c7cec1233d76a395d2a
jpramirezlo <jramirez2803@gmail.com>
2021-06-13 18:21:26 -0500

    Practicas Proyecto

---
+++ src/test/java/starter/stepdefinitions/Login.java
@@ -0,0 +1,24 @@
+package starter.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login {
+
+    @Given("(.*) has an active account")
+    public void sergey_has_an_active_account(String name) {
+
+    }
+
+    @When("he sends their valid credentials")
+    public void he_sends_their_valid_credentials() {
+
+    }
+
+    @Then("he should have access to manage his account")
+    public void he_should_have_access_to_manage_his_account() {
+
+    }
+
+}
